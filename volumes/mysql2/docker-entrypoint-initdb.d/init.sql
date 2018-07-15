bc0ff0a3dab00eb2a84ef964554d97a07d1387fb
mrasu <m.rasu.hitsuji@gmail.com>
2018-07-15 20:11:05 +0900

    Add init sqls

---
+++ volumes/mysql2/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE cowloon;
+USE cowloon;
+CREATE TABLE messages(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255));
+
+INSERT INTO messages(text) values('PRESENT DAY'), ('PRESENT TIME'), (NULL);

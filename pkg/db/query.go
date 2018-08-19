2806f1362348a5278b6195ad13ef93ecd9dc17ee
mrasu <m.rasu.hitsuji@gmail.com>
2018-08-19 19:29:53 +0900

    Implement migration for UPDATE and DELETE

--- pkg/db/query.go
+++ pkg/db/query.go
@@ -19,7 +19,11 @@ func NewQuery(sql string, args []interface{}) *Query {
 	}
 }
 
-func BuildInsertQuery(tableName string, columns []schema.TableColumn, args []interface{}) *Query {
+func BuildInsertQuery(tableName string, columns []schema.TableColumn, args []interface{}) (*Query, error) {
+	if len(columns) != len(args) {
+		return nil, fmt.Errorf("clauses for INSERT don't have same length, (columns: %d, args: %d)", len(columns), len(args))
+	}
+
 	var cNames []string
 	var ph []string
 	for _, column := range columns {
@@ -28,5 +32,52 @@ func BuildInsertQuery(tableName string, columns []schema.TableColumn, args []int
 	}
 
 	sql := fmt.Sprintf("REPLACE INTO %s (%s) VALUES (%s)", tableName, strings.Join(cNames, ", "), strings.Join(ph, ", "))
-	return NewQuery(sql, args)
+	return NewQuery(sql, args), nil
+}
+
+func BuildUpdateQuery(tableName string, columns []schema.TableColumn, whereValues []interface{}, setValues []interface{}) (*Query, error) {
+	if len(columns) != len(whereValues) || len(columns) != len(setValues) {
+		return nil, fmt.Errorf("clauses for UPDATE don't have same length, (columns: %d, where: %d, update: %d)", len(columns), len(whereValues), len(setValues))
+	}
+	whereClause := buildPreparedWhereClause(columns)
+	setClause := buildPreparedSetClause(columns)
+
+	sql := fmt.Sprintf("UPDATE %s SET %s WHERE %s ", tableName, setClause, whereClause)
+
+	var args []interface{}
+	for _, val := range setValues {
+		args = append(args, val)
+	}
+	for _, val := range whereValues {
+		args = append(args, val)
+	}
+
+	return NewQuery(sql, args), nil
+}
+
+func buildPreparedWhereClause(columns []schema.TableColumn) string {
+	var whereClauses []string
+	for _, column := range columns {
+		whereClauses = append(whereClauses, column.Name+"=?")
+	}
+	return strings.Join(whereClauses, " AND ")
+}
+
+func buildPreparedSetClause(columns []schema.TableColumn) string {
+	var whereClauses []string
+	for _, column := range columns {
+		whereClauses = append(whereClauses, column.Name+"=?")
+	}
+	return strings.Join(whereClauses, ",")
+}
+
+func BuildDeleteQuery(tableName string, columns []schema.TableColumn, args []interface{}) (*Query, error) {
+	if len(columns) != len(args) {
+		return nil, fmt.Errorf("clauses for DELETE don't have same length, (columns: %d, where: %d)", len(columns), len(args))
+	}
+
+	whereClause := buildPreparedWhereClause(columns)
+
+	sql := fmt.Sprintf("DELETE FROM %s WHERE %s", tableName, whereClause)
+	return NewQuery(sql, args), nil
 }
--- pkg/migrator/canal_handler.go
+++ pkg/migrator/canal_handler.go
@@ -1,6 +1,8 @@
 package migrator
 
 import (
+	"fmt"
+
 	"github.com/mrasu/Cowloon/pkg/db"
 	"github.com/siddontang/go-mysql/canal"
 )
@@ -23,18 +25,44 @@ func (c *CanalHandler) OnRow(ev *canal.RowsEvent) error {
 		return nil
 	}
 
-	queries := c.toQuery(ev)
+	queries, err := c.toQuery(ev)
+	if err != nil {
+		return err
+	}
+
 	c.dmlEventChan <- queries
 	return nil
 }
 
-func (c *CanalHandler) toQuery(ev *canal.RowsEvent) []*db.Query {
+func (c *CanalHandler) toQuery(ev *canal.RowsEvent) ([]*db.Query, error) {
 	var queries []*db.Query
 	if ev.Action == canal.InsertAction {
 		for _, row := range ev.Rows {
-			queries = append(queries, db.BuildInsertQuery(ev.Table.Name, ev.Table.Columns, row))
+			query, err := db.BuildInsertQuery(ev.Table.Name, ev.Table.Columns, row)
+			if err != nil {
+				return nil, err
+			}
+			queries = append(queries, query)
+		}
+	} else if ev.Action == canal.UpdateAction {
+		if len(ev.Rows) != 2 {
+			return nil, fmt.Errorf("update event doesn't have two rows (rows: %d)", len(ev.Rows))
+		}
+
+		query, err := db.BuildUpdateQuery(ev.Table.Name, ev.Table.Columns, ev.Rows[0], ev.Rows[1])
+		if err != nil {
+			return nil, err
+		}
+		queries = append(queries, query)
+	} else if ev.Action == canal.DeleteAction {
+		for _, row := range ev.Rows {
+			query, err := db.BuildDeleteQuery(ev.Table.Name, ev.Table.Columns, row)
+			if err != nil {
+				return nil, err
+			}
+			queries = append(queries, query)
 		}
 	}
 
-	return queries
+	return queries, nil
 }
